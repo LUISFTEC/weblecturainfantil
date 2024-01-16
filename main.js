d83675702a2b61a814d622e3e1947f98b3236816
luisftec <luisftec2022@gmail.com>
2024-01-16 09:00:42 -0500

    FIN entrega

--- main.js
+++
@@ -1,9 +0,0 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-
-import { auth } from './script/firebase.js'
-
-console.log ('hello wordl')
-
-onAuthStateChanged(auth, async (user) =>{
-    console.log(user)
-})
